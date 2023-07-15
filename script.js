0745d5302f7963db564b8367ada536193be40067
Ankit <jainank548@gmail.com>
2023-07-15 11:40:14 +0530

    'sumbitchanged'

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ let questions_array = [
 
 ];
 
+
+let ans_array = [];
 let question = document.getElementsByClassName('question')[0].firstElementChild;
 let option = document.getElementsByTagName('label');
 let submit = document.getElementsByClassName('submit')[0];
@@ -54,7 +56,7 @@ let forwardbtn = document.getElementsByClassName('forwardbtn')[0];
 let options = document.getElementsByClassName('options')[0].innerHTML;
 
 let index = 0;
-let attempt = 0;
+let attempt;
 let right = 0;
 let total = questions_array.length;
 let ans;
@@ -62,7 +64,8 @@ let data;
 function loadquestion() {
 
     if (index == (questions_array.length)) {
-        let final = confirm(`Do you really want to Submit?\nYou attemted ${attempt} out of ${total-1}`)
+        attempted()
+        let final = confirm(`Do you really want to Submit?\nYou attemted ${attempt.length} out of ${total-1}`)
         if(final){
             return quizend()
         }
@@ -72,9 +75,11 @@ function loadquestion() {
 
         // return quizend()
     }
-    if (questions_array.length==0) {
-        alert('You have attempted all questions');
-        return quizend()
+
+    if(index==0){
+        backbtn.style.display='none'
+    }else{
+        backbtn.style.display='inline'
     }
     
     reset()
@@ -83,9 +88,13 @@ function loadquestion() {
 
     if (index == questions_array.length - 1) {
         document.getElementsByClassName('options')[0].innerHTML='';
+        submit.style.display='inline';
+        forwardbtn.style.display='none'
     }
     else{
         document.getElementsByClassName('options')[0].innerHTML=options;
+        submit.style.display='none';
+        forwardbtn.style.display='inline'
         option[0].lastElementChild.innerHTML = data["Option1"];
         option[1].lastElementChild.innerHTML = data["Option2"];
         option[2].lastElementChild.innerHTML = data["Option3"];
@@ -101,12 +110,7 @@ forwardbtn.addEventListener('click', forward)
 
 
 function getAnswer() {
-    if (index == questions_array.length - 1) {
-    } else {
-        answer()
-        attempt++;
-    }
-    questions_array.splice(index, 1)
+    index++;
     loadquestion()
 }
 
@@ -114,46 +118,56 @@ function answer() {
     Array.from(option).map((value, ind) => {
 
         if (option[ind].firstElementChild.checked) {
-            ans = value.lastElementChild.innerHTML;
+            ans = value.firstElementChild.value;
+            ans_array[index]=ans;
         }
 
-    })
 
-    data = questions_array[index];
-    if (ans === data["Correct"]) {
-        right++
-    }
+    })
 }
 
 function quizend() {
     document.getElementsByClassName('container')[0].innerHTML = `
     <div><h2 style="margin-bottom: 20px;">The Quiz Ended </h2></div>
-    <div><h2 style="margin-bottom: 20px;">You have attempted ${attempt} out of ${total - 1}</h2></div>
+    <div><h2 style="margin-bottom: 20px;">You have attempted ${attempt.length} out of ${total - 1}</h2></div>
     <div><h2>Correct Answers ${right} out of ${total - 1}</h2></div>`
 }
 
 function reset() {
     Array.from(option).map((value) => {
         value.firstElementChild.checked = false;
-
     })
 }
 
 
 function back() {
     if (index > 0 && index < questions_array.length) {
-        // console.log('back')
+        answer()
         index--
         loadquestion()
+        check()
     }
 }
 
 
 function forward() {
     if (index >= 0 && index < (questions_array.length - 1)) {
-        // console.log('forward')
+        answer()
         index++
-        // console.log(index)
         loadquestion()
+        check()
     }
 }
+
+function check(){
+        if (ans_array[index]!=null) {
+            let a = document.querySelector(`input[value="${ans_array[index]}"]`).checked=true
+        }
+        
+}
+function attempted(){
+     attempt = ans_array.filter((elm)=>{
+        return (elm != null && elm !== false && elm !== "")
+    })
+}
+
